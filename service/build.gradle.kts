a8d1bfc243bd53afc8f8ca9c56d051563d3084a8
lliioollzh <lliiooll@126.com>
2023-12-10 15:53:03 +0800

    interface

--- service/build.gradle.kts
+++ service/build.gradle.kts
@@ -1,7 +1,5 @@
 plugins {
     id("com.android.library")
-    id("maven-publish")
-    id("signing")
 }
 
 android {
@@ -36,60 +34,3 @@ dependencies {
     compileOnly("androidx.annotation:annotation:1.7.0")
 }
 
-publishing {
-    publications {
-        register<MavenPublication>("service") {
-            artifactId = "service"
-            group = "io.github.libxposed"
-            version = "100-1.0.0"
-            pom {
-                name.set("service")
-                description.set("Modern Xposed Service Interface")
-                url.set("https://github.com/libxposed/service")
-                licenses {
-                    license {
-                        name.set("Apache License 2.0")
-                        url.set("https://github.com/libxposed/service/blob/master/LICENSE")
-                    }
-                }
-                developers {
-                    developer {
-                        name.set("libxposed")
-                        url.set("https://libxposed.github.io")
-                    }
-                }
-                scm {
-                    connection.set("scm:git:https://github.com/libxposed/service.git")
-                    url.set("https://github.com/libxposed/service")
-                }
-            }
-            afterEvaluate {
-                from(components.getByName("release"))
-            }
-        }
-    }
-    repositories {
-        maven {
-            name = "ossrh"
-            url = uri("https://s01.oss.sonatype.org/service/local/staging/deploy/maven2/")
-            credentials(PasswordCredentials::class)
-        }
-        maven {
-            name = "GitHubPackages"
-            url = uri("https://maven.pkg.github.com/libxposed/service")
-            credentials {
-                username = System.getenv("GITHUB_ACTOR")
-                password = System.getenv("GITHUB_TOKEN")
-            }
-        }
-    }
-}
-
-signing {
-    val signingKey = findProperty("signingKey") as String?
-    val signingPassword = findProperty("signingPassword") as String?
-    if (!signingKey.isNullOrBlank() && !signingPassword.isNullOrBlank()) {
-        useInMemoryPgpKeys(signingKey, signingPassword)
-        sign(publishing.publications)
-    }
-}
